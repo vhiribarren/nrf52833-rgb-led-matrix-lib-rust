bd6052a3bace4bec0182a449876b5c2fcb788845
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-12-29 18:30:39 +0100

    Method to correctly latch lines

--- src/ledmatrix.rs
+++ src/ledmatrix.rs
@@ -1,18 +1,70 @@
 use microbit::hal::gpio::{Level, Output, Pin, PushPull};
 use microbit::hal::prelude::*;
 
-pub struct LedMatrix {
+pub struct LedMatrixPins64x32 {
+    pub r1: Pin<Output<PushPull>>,
+    pub g1: Pin<Output<PushPull>>,
+    pub b1: Pin<Output<PushPull>>,
+    pub r2: Pin<Output<PushPull>>,
+    pub g2: Pin<Output<PushPull>>,
+    pub b2: Pin<Output<PushPull>>,
+    pub a: Pin<Output<PushPull>>,
+    pub b: Pin<Output<PushPull>>,
+    pub c: Pin<Output<PushPull>>,
+    pub d: Pin<Output<PushPull>>,
+    pub clk: Pin<Output<PushPull>>,
+    pub lat: Pin<Output<PushPull>>,
+    pub oe: Pin<Output<PushPull>>,
+}
+
+pub struct LedMatrix<const N: usize = 4> {
     pub pin_r1: Pin<Output<PushPull>>,
     pub pin_g1: Pin<Output<PushPull>>,
     pub pin_b1: Pin<Output<PushPull>>,
     pub pin_r2: Pin<Output<PushPull>>,
     pub pin_g2: Pin<Output<PushPull>>,
     pub pin_b2: Pin<Output<PushPull>>,
-    pub pin_a: Pin<Output<PushPull>>,
-    pub pin_b: Pin<Output<PushPull>>,
-    pub pin_c: Pin<Output<PushPull>>,
-    pub pin_d: Pin<Output<PushPull>>,
     pub pin_clk: Pin<Output<PushPull>>,
-    pub pin_lat: Pin<Output<PushPull>>,
-    pub pin_oe: Pin<Output<PushPull>>,
+    pin_lat: Pin<Output<PushPull>>,
+    pin_oe: Pin<Output<PushPull>>,
+    line_ctrl: [Pin<Output<PushPull>>; N],
+}
+
+impl LedMatrix {
+    pub fn new(pins: LedMatrixPins64x32) -> LedMatrix {
+        LedMatrix {
+            pin_r1: pins.r1.into_push_pull_output(Level::Low),
+            pin_g1: pins.g1.into_push_pull_output(Level::Low),
+            pin_b1: pins.b1.into_push_pull_output(Level::Low),
+            pin_r2: pins.r2.into_push_pull_output(Level::Low),
+            pin_g2: pins.g2.into_push_pull_output(Level::Low),
+            pin_b2: pins.b2.into_push_pull_output(Level::Low),
+            pin_clk: pins.clk.into_push_pull_output(Level::Low),
+            pin_lat: pins.lat.into_push_pull_output(Level::Low),
+            pin_oe: pins.oe.into_push_pull_output(Level::Low),
+            line_ctrl: [
+                pins.a.into_push_pull_output(Level::Low),
+                pins.b.into_push_pull_output(Level::Low),
+                pins.c.into_push_pull_output(Level::Low),
+                pins.d.into_push_pull_output(Level::Low),
+            ],
+        }
+    }
+}
+
+impl<const N: usize> LedMatrix<N> {
+    pub fn draw_canvas() {}
+    pub fn latch_to_line(&mut self, line: u8) {
+        self.pin_oe.set_high().unwrap();
+        let mline = line % 2_u8.pow(N as u32);
+        for pin_idx in 0..self.line_ctrl.len() {
+            let enable_pin = (mline & (1 << pin_idx)) != 0;
+            self.line_ctrl[pin_idx]
+                .set_state(PinState::from(enable_pin))
+                .unwrap();
+        }
+        self.pin_lat.set_high().unwrap();
+        self.pin_lat.set_low().unwrap();
+        self.pin_oe.set_low().unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,16 @@ use cortex_m_rt::entry;
 use microbit::board::Board;
 use microbit::hal::gpio::Level;
 use microbit::hal::prelude::*;
-use microbit::hal::timer::Timer;
-use microbit_led_matrix::ledmatrix::LedMatrix;
+use microbit_led_matrix::ledmatrix::{LedMatrix, LedMatrixPins64x32};
 use panic_halt as _;
-use rtt_target::rtt_init_print;
-
-const DELAY: u32 = 1000;
 
 /*
-
 If there are not regular switch between two elements of A, B, C or D, the panel shutdown.
 A minimal block like this one is needed to allow the system working:
    loop {
         pin_a.set_low().unwrap();
         pin_a.set_high().unwrap();
    }
-
 
 Correct order is:
     for _ in range(64):
@@ -33,109 +27,66 @@ Correct order is:
     set latch to H
     set latch to L
     set OE to L
-
 */
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-
     let board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
 
-    let mut m = LedMatrix {
-        pin_r1: board.pins.p0_02.into_push_pull_output(Level::Low).into(),
-        pin_g1: board.pins.p0_03.into_push_pull_output(Level::Low).into(),
-        pin_b1: board.pins.p0_04.into_push_pull_output(Level::Low).into(),
-        pin_r2: board
-            .display_pins
-            .col3
-            .into_push_pull_output(Level::Low)
-            .into(),
-        pin_g2: board
-            .display_pins
-            .col1
-            .into_push_pull_output(Level::Low)
-            .into(),
-        pin_b2: board
+    let mut m = LedMatrix::new(LedMatrixPins64x32 {
+        r1: board.pins.p0_02.into_push_pull_output(Level::Low).into(),
+        g1: board.pins.p0_03.into_push_pull_output(Level::Low).into(),
+        b1: board.pins.p0_04.into_push_pull_output(Level::Low).into(),
+        r2: board.display_pins.col3.into(),
+        g2: board.display_pins.col1.into(),
+        b2: board
             .buttons
             .button_a
             .into_push_pull_output(Level::Low)
             .into(),
-        pin_clk: board
-            .display_pins
-            .col5
-            .into_push_pull_output(Level::Low)
-            .into(),
-        pin_lat: board
+        clk: board.display_pins.col5.into(),
+        lat: board
             .buttons
             .button_b
             .into_push_pull_output(Level::Low)
             .into(),
-        pin_oe: board.pins.p0_12.into_push_pull_output(Level::Low).into(),
-        pin_a: board
-            .display_pins
-            .col4
-            .into_push_pull_output(Level::Low)
-            .into(),
-        pin_b: board
-            .display_pins
-            .col2
-            .into_push_pull_output(Level::Low)
-            .into(),
-        pin_c: board.pins.p0_10.into_push_pull_output(Level::Low).into(),
-        pin_d: board.pins.p0_09.into_push_pull_output(Level::Low).into(),
-    };
+        oe: board.pins.p0_12.into_push_pull_output(Level::Low).into(),
+        a: board.display_pins.col4.into(),
+        b: board.display_pins.col2.into(),
+        c: board.pins.p0_10.into_push_pull_output(Level::Low).into(),
+        d: board.pins.p0_09.into_push_pull_output(Level::Low).into(),
+    });
+
+    for col in 0..64 {
+        if col < 64 / 3 {
+            m.pin_r1.set_low().unwrap();
+            m.pin_g1.set_low().unwrap();
+            m.pin_b1.set_high().unwrap();
+            m.pin_r2.set_low().unwrap();
+            m.pin_g2.set_low().unwrap();
+            m.pin_b2.set_high().unwrap();
+        } else if col > 2 * 64 / 3 {
+            m.pin_r1.set_high().unwrap();
+            m.pin_g1.set_low().unwrap();
+            m.pin_b1.set_low().unwrap();
+            m.pin_r2.set_high().unwrap();
+            m.pin_g2.set_low().unwrap();
+            m.pin_b2.set_low().unwrap();
+        } else {
+            m.pin_r1.set_high().unwrap();
+            m.pin_g1.set_high().unwrap();
+            m.pin_b1.set_high().unwrap();
+            m.pin_r2.set_high().unwrap();
+            m.pin_g2.set_high().unwrap();
+            m.pin_b2.set_high().unwrap();
+        }
+        m.pin_clk.set_high().unwrap();
+        m.pin_clk.set_low().unwrap();
+    }
 
     loop {
-        m.pin_r1.set_low().unwrap();
-        m.pin_g1.set_low().unwrap();
-        m.pin_b1.set_low().unwrap();
-        m.pin_r2.set_low().unwrap();
-        m.pin_g2.set_low().unwrap();
-        m.pin_b2.set_high().unwrap();
-        for idx in 0..64 {
-            if idx < 63 {
-                m.pin_r1.set_low().unwrap();
-            } else {
-                m.pin_r1.set_high().unwrap();
-            }
-            m.pin_clk.set_high().unwrap();
-            m.pin_clk.set_low().unwrap();
+        for line in 0..16 {
+            m.latch_to_line(line);
         }
-        m.pin_oe.set_high().unwrap();
-        m.pin_a.set_low().unwrap();
-        m.pin_b.set_low().unwrap();
-        m.pin_c.set_low().unwrap();
-        m.pin_d.set_low().unwrap();
-        m.pin_oe.set_low().unwrap();
-        m.pin_oe.set_high().unwrap();
-        m.pin_lat.set_high().unwrap();
-        m.pin_lat.set_low().unwrap();
-        m.pin_oe.set_low().unwrap();
-
-        m.pin_r1.set_low().unwrap();
-        m.pin_g1.set_low().unwrap();
-        m.pin_b1.set_low().unwrap();
-        m.pin_r2.set_low().unwrap();
-        m.pin_g2.set_low().unwrap();
-        m.pin_b2.set_low().unwrap();
-        for idx in 0..64 {
-            if idx < 2 {
-                m.pin_b2.set_low().unwrap();
-            } else {
-                m.pin_b2.set_high().unwrap();
-            }
-            m.pin_clk.set_high().unwrap();
-            m.pin_clk.set_low().unwrap();
-        }
-        m.pin_oe.set_high().unwrap();
-        m.pin_a.set_low().unwrap();
-        m.pin_b.set_low().unwrap();
-        m.pin_c.set_low().unwrap();
-        m.pin_d.set_high().unwrap();
-        m.pin_lat.set_high().unwrap();
-        m.pin_lat.set_low().unwrap();
-        m.pin_oe.set_low().unwrap();
     }
 }
